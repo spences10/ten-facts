65825c3aa29e9581d781973dd6d72a3356523ddd
spences10 <spences10apps@gmail.com>
2018-08-14 17:07:00 +0100

    add sc button

---
+++ components/Button.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  color: red;
+  font-size: 1rem;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+  border: 1px solid black;
+  background-color: Transparent;
+  text-transform: uppercase;
+  border-radius: 4px;
+  transition: all 0.1s;
+  &:hover {
+    transform: translateY(1px);
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+  }
+`;
