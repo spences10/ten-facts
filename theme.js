6217620686e0e1b29fdfae50a0b48fb7adcf770d
spences10 <spences10apps@gmail.com>
2018-08-15 07:13:04 +0100

    custom theme

---
+++ theme.js
@@ -0,0 +1,14 @@
+import theme from 'mdx-deck/themes'
+
+export default {
+  // extends the default theme
+  ...theme,
+  // add a custom font
+  font: 'Dank Mono, Roboto, sans-serif',
+  // custom colors
+  colors: {
+    text: '#34434b',
+    background: '#fafafa',
+    link: '#fof'
+  }
+}
