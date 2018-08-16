8c20270219eef4f6415ebbb6ab2a7506989bc097
spences10 <spences10apps@gmail.com>
2018-08-16 05:49:33 +0100

    add webfont

--- theme.js
+++ theme.js
@@ -1,10 +1,11 @@
 import theme from 'mdx-deck/themes'
+import './font'
 
 export default {
   // extends the default theme
   ...theme,
   // add a custom font
-  font: 'Dank Mono, Roboto, sans-serif',
+  font: 'dm, Roboto, sans-serif',
   // custom colors
   colors: {
     text: '#34434b',
